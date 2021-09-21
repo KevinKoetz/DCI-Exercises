35091d2fbef8397881c98396ea2670559744713a
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-09-21 11:35:59 +0200

    solved

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+/* console.log("This is my first js file"); */
+
+/* 
+Remove a parenthesis from the first statement and run the code. Does your statement print?
+No, parenthesis are needed.
+Is there an error? If so, what kind of error is it?
+Syntax Error
+*/
