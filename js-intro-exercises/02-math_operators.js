544e1c35802d2b491a35315919de9075db104617
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-09-21 13:16:43 +0200

    Clone exercise

---
+++ js-intro-exercises/02-math_operators.js
@@ -0,0 +1,49 @@
+/**
+ * Exercise 01
+ * Write a function toFahrenheit that converts a temperature from Celsius to Fahrenheit.
+ * Example: toFahrenheit(0) should return 32.
+ */
+let toFahrenheit = (celsius) => celsius * 9/5 + 32;
+console.log(toFahrenheit(0));
+console.log(toFahrenheit(-273.15));
+console.log(toFahrenheit(1000));
+console.log(toFahrenheit(100));
+
+/**
+ * Exercise 02
+ * Write a function digits that takes a natural number as input and 
+ * returns the number of digits of that number.
+ * Example: digits(2674) should return 4.
+ */
+
+let digits = (num) => ;
+console.log(digits(1234));
+console.log(digits(1));
+
+/**
+ * Exercise 03
+ * Which value does x have after execution of the following code?
+ */
+ let x = 3;
+ x++;
+ x = x * 2;
+ x--;
+
+ /**
+  * Exercise 04
+  * Write a function mean that takes 2 numbers and returns their mean value.
+  * Example: mean(1, 2) should return 1.5.
+  */
+
+
+ /**
+  * Exercise 05
+  * Write a JavaScript function to check whether its input parameter is a string or not 
+  * and prints the result to the console
+  */
+
+ /**
+  * Exercise 06
+  * Write a JavaScript program to find the area of a triangle.
+  * the formula is base * height / 2
+  */
