7b0b54d6ca7fef36be5b6a53a70172029253442c
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-09-22 10:58:47 +0200

    solved

---
+++ main.js
@@ -0,0 +1,10 @@
+let introSentence = (age) => `hello, my name is Fran and I am ${age}`;
+let age = 25;
+
+console.log(introSentence(age));
+
+let v1 = "1005";
+let v2 = "10.05";
+
+console.log(Number(v1));
+console.log(Number(v2));
