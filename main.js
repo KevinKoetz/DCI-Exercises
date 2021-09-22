46266ffd45b38b005f12437c92532be292a80e51
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-09-22 14:16:49 +0200

    solved

--- main.js
+++ main.js
@@ -7,8 +7,19 @@ function vowels(str) {
 }
 
 
+
+function vowelsRegEx(str) {
+    return  str.match(/[aeiou]/g)?.join("") ?? "" ;
+}
+
 console.log(vowels("aeiou"));
 console.log(vowels(""));
 console.log(vowels("ZZZazzz"));
 console.log(vowels("ZZZ"));
 console.log(vowels("This is a test string"));
+
+console.log(vowelsRegEx("aeiou"));
+console.log(vowelsRegEx(""));
+console.log(vowelsRegEx("ZZZazzz"));
+console.log(vowelsRegEx("ZZZ"));
+console.log(vowelsRegEx("This is a test string"));
