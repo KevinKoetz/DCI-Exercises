8a2e03a12f303d750004b0c14eebd41acade17d5
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-09-22 15:34:50 +0200

    l

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function fibonacciRec(n) {
-    if(n === 0) return 0;
-    if(n === 1) return 1;
+    if(n === 1) return 0;
+    if(n === 2) return 1;
     return fibonacciRec(n-1) + fibonacciRec(n-2);
 }
 
 function fibonacci(n) {
-    if(n === 0) return 0;
-    if(n === 1) return 1;
+    if(n === 1) return 0;
+    if(n === 2) return 1;
     let n1 = 0;
     let n2 = 1;
     let result = n1+n2;
-    for(let i = 2; i <= n; i++){
+    for(let i = 3; i <= n; i++){
         result = n1 + n2;
         n1 = n2;
         n2 = result;
@@ -18,8 +18,8 @@ function fibonacci(n) {
     return result;
 }
 
-for(let i = 0; i <= 50; i++) console.log(fibonacci(i));
-for(let i = 0; i <= 50; i++) console.log(fibonacciRec(i));
+for(let i = 1; i <= 20; i++) console.log(fibonacci(i));
+for(let i = 1; i <= 20; i++) console.log(fibonacciRec(i));
 
 
 
