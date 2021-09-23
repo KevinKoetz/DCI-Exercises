2128699753944eb499af5cc68526f68a358eed00
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-09-23 12:45:10 +0200

    add xbonacci

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 function fibonacciRec(n) {
-    if(n === 1) return 0;
-    if(n === 2) return 1;
-    return fibonacciRec(n-1) + fibonacciRec(n-2);
+  if (n === 1) return 0;
+  if (n === 2) return 1;
+  return fibonacciRec(n - 1) + fibonacciRec(n - 2);
 }
 
 function fibonacci(n) {
-    if(n === 1) return 0;
-    if(n === 2) return 1;
-    let n1 = 0;
-    let n2 = 1;
-    let result = n1+n2;
-    for(let i = 3; i <= n; i++){
-        result = n1 + n2;
-        n1 = n2;
-        n2 = result;
-    }
-    return result;
+  if (n === 1) return 0;
+  if (n === 2) return 1;
+  let n1 = 0;
+  let n2 = 1;
+  let result = n1 + n2;
+  for (let i = 3; i <= n; i++) {
+    result = n1 + n2;
+    n1 = n2;
+    n2 = result;
+  }
+  return result;
 }
 
-for(let i = 1; i <= 20; i++) console.log(fibonacci(i));
-for(let i = 1; i <= 20; i++) console.log(fibonacciRec(i));
+function xbonacci(signature, n) {
+  let sequence = [];
+  for (let i = 0; i < n; i++) {
+    if (i < signature.length) {
+      sequence.push(signature[i]);
+    } else {
+      sequence.push(
+        sequence.slice(-signature.length).reduce((prev, cur) => prev + cur)
+      );
+    }
+  }
+  return sequence;
+}
 
+for (let i = 1; i <= 20; i++) console.log(fibonacci(i));
+for (let i = 1; i <= 20; i++) console.log(fibonacciRec(i));
 
-
+console.log(xbonacci([1, 1, 1],20));
