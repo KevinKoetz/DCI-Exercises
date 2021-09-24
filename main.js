43ec77b6161ee49e5a490e335460c4f511bd0496
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-09-24 16:28:32 +0200

    add case insensitivity, non-letter ignorance and testcases

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-function anagram(s1,s2) {   
+function anagram(s1,s2) {
+    //Make it Case insensitive   
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
+    //Remove all non-letter characters
+    s1 = s1.match(/[a-z]+/g)?.join("") ?? null;
+    s2 = s2.match(/[a-z]+/g)?.join("") ?? null;
+
+    //Return false if any of the Strings contains only non-letter characters
+    if(!(s1 && s2)) return false;
+
+    //Shortcut, return false if both strings have different length (they can not be anagrams)
+    if(s1.length !== s2.length) return false;
+
     //Count the occourence of each different letter in s1 and store them in an object.
     let letterCount = Array.from(s1).reduce((prev, curr)=>{
         prev[curr] === undefined ? prev[curr] = 1 : prev[curr]++;
@@ -19,5 +33,29 @@ function anagram(s1,s2) {
     return true;
 }
 
-console.log(anagram("",""));
-console.log(anagram("dadd","dda8d"));
+
+
+
+//Testcases:
+//Same Letter should be an anagram
+console.log("Expected true:",anagram("s","s"));
+
+//different Letter should not be an anagram
+console.log("Expected false:",anagram("s","x"));
+
+//Casing should not matter
+console.log("Expected true:",anagram("S","s"));
+
+//consider only characters, not spaces or punctuation.
+console.log("Expected true:",anagram(" s","s."));
+console.log("Expected false:",anagram("",""));
+console.log("Expected false:",anagram(" "," "));
+
+//Some random anagrams
+console.log("Expected true:",anagram("sdqqsag","sgasqqd"));
+console.log("Expected true:",anagram("xwWSfFF","sffXwwF"));
+
+//Some random not-anagrams
+console.log("Expected false:",anagram("awdasf","a2fasg"));
+console.log("Expected false:",anagram("aawfawt","gawsdax"));
+
