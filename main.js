2e6c533896c670062b420a8352294c2bfce873fe
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-09-25 19:57:52 +0200

    a

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 function fibonacciRec(n) {
+  if (n <= 0) return NaN;
   if (n === 1) return 0;
   if (n === 2) return 1;
   return fibonacciRec(n - 1) + fibonacciRec(n - 2);
 }
 
 function fibonacci(n) {
+  if (n <= 0) return NaN;
   if (n === 1) return 0;
   if (n === 2) return 1;
   let n1 = 0;
@@ -32,7 +34,18 @@ function xbonacci(signature, n) {
   return sequence;
 }
 
+
+function xbonacciRec(signature, n) {
+    if(n <= signature.length) return signature[n-1]
+    let result = 0;
+    for(let i = 1; i <= signature.length;i++){
+        result += xbonacciRec(signature, n-i);
+    }
+    return result;
+}
+
 for (let i = 1; i <= 20; i++) console.log(fibonacci(i));
 for (let i = 1; i <= 20; i++) console.log(fibonacciRec(i));
 
-console.log(xbonacci([1, 1, 1],20));
+console.log(xbonacci([0,1,5,10],40));
+console.log(xbonacciRec([0,1,5,10],40));
