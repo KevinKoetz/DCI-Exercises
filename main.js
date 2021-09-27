69106e53f3b4000c3fc0e282556675d8c09e626b
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-09-27 15:07:10 +0200

    solve

--- main.js
+++ main.js
@@ -9,8 +9,43 @@ let v = true;
 console.log(!v ? "true option" : "false option");
 // expression ? returnIfTrue : returnIfFalse <-- if expression is truthy return "returnIfTrue" otherwise "returnIfFalse"
 
-let num = 3;
+let num = 1;
 
 console.log(
-  num % 3 || num % 3) ? "FizzBuzz"  : num
+    num % 3 == 0 && num % 5 == 0
+    ? "FizzBuzz"
+    : num % 3 == 0
+    ? "Fizz"
+    : num % 5 == 0
+    ? "Buzz"
+    : num
 );
+
+let firstName, givenName;
+
+firstName = 'Stacey';
+let name = givenName || firstName || 'John'; 
+
+console.log("OR short circuit", name);
+
+
+firstName, givenName;
+
+givenName = true;
+firstName = 'Stacey';
+name = givenName && firstName && null; 
+
+console.log("AND short circuit", name);
+
+
+
+function divide(a, b) {
+    if(b){
+        return a / b;
+    }else{
+        return undefined
+    }
+}
+
+console.log(divide(10,0));
+
