394f7124a11c75e9bbb9824a63c3d26a21e692bb
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-10-12 13:25:11 +0200

    solved

---
+++ main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * Julius Caesar protected his confidential information by encrypting it using a cipher. 
+ * Caesar's cipher shifts each letter by a number of letters. 
+ * If the shift takes you past the end of the alphabet, 
+ * just rotate back to the front of the alphabet. 
+ * In the case of a rotation by 3, w, x, y and z would map to z, a, b and c
+ * Examples:
+ * caesarCipher('middle-Outz',2) -> okffng-Qwvb
+ * caesarCipher('Always-Look-on-the-Bright-Side-of-Life', 5) -> Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj
+ * caesarCipher('www.abc.xy', 87) -> fff.jkl.gh
+ * the rotation can be any integer between 1 and 100
+ */
+
+const caesarCipher = (str, shift) => {
+  let abc = "abcdefghijklmnopqrstuvwxyz"
+  let arr = str.split("")
+
+  return arr.map((item) => {
+
+    if(abc.includes(item.toLowerCase())){
+
+      if(item.toLowerCase() === item){
+
+        return abc[(abc.indexOf(item) + shift) % (abc.length)]
+
+      }else{
+
+        return abc.toUpperCase()[(abc.toUpperCase().indexOf(item) + shift) % (abc.length)]
+
+      }
+    }else {
+
+      return item;
+
+    }
+  }).join("")
+}
+
+
+console.log(caesarCipher('middle-Outz',2) === "okffng-Qwvb") 
+console.log(caesarCipher('Always-Look-on-the-Bright-Side-of-Life', 5) === "Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj")
+console.log(caesarCipher('www.abc.xy', 87) === "fff.jkl.gh")
