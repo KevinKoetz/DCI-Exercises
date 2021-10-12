7bdac1aacca686b1607cfbeb9245e6a6edafeeee
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-10-12 13:38:05 +0200

    Implemented Left shift with -shift value

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 /**
- * Julius Caesar protected his confidential information by encrypting it using a cipher. 
- * Caesar's cipher shifts each letter by a number of letters. 
- * If the shift takes you past the end of the alphabet, 
- * just rotate back to the front of the alphabet. 
+ * Julius Caesar protected his confidential information by encrypting it using a cipher.
+ * Caesar's cipher shifts each letter by a number of letters.
+ * If the shift takes you past the end of the alphabet,
+ * just rotate back to the front of the alphabet.
  * In the case of a rotation by 3, w, x, y and z would map to z, a, b and c
  * Examples:
  * caesarCipher('middle-Outz',2) -> okffng-Qwvb
@@ -14,31 +14,32 @@
  */
 
 const caesarCipher = (str, shift) => {
-  let abc = "abcdefghijklmnopqrstuvwxyz"
-  let arr = str.split("")
+  let abc = shift >= 0 ? "abcdefghijklmnopqrstuvwxyz" : "abcdefghijklmnopqrstuvwxyz".split("").reverse().join("");
+  let arr = str.split("");
+  shift = Math.abs(shift)
 
-  return arr.map((item) => {
-
-    if(abc.includes(item.toLowerCase())){
-
-      if(item.toLowerCase() === item){
-
-        return abc[(abc.indexOf(item) + shift) % (abc.length)]
-
-      }else{
-
-        return abc.toUpperCase()[(abc.toUpperCase().indexOf(item) + shift) % (abc.length)]
-
+  return arr
+    .map((item) => {
+      if (abc.includes(item.toLowerCase())) {
+        if (item.toLowerCase() === item) {
+          return abc[(abc.indexOf(item) + shift) % abc.length];
+        } else {
+          return abc.toUpperCase()[
+            (abc.toUpperCase().indexOf(item) + shift) % abc.length
+          ];
+        }
+      } else {
+        return item;
       }
-    }else {
+    })
+    .join("");
+};
 
-      return item;
+console.log(caesarCipher("middle-Outz", 2) === "okffng-Qwvb");
+console.log(
+  caesarCipher("Always-Look-on-the-Bright-Side-of-Life", 5) ===
+    "Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj"
+);
+console.log(caesarCipher("www.abc.xy", 87) === "fff.jkl.gh");
 
-    }
-  }).join("")
-}
-
-
-console.log(caesarCipher('middle-Outz',2) === "okffng-Qwvb") 
-console.log(caesarCipher('Always-Look-on-the-Bright-Side-of-Life', 5) === "Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj")
-console.log(caesarCipher('www.abc.xy', 87) === "fff.jkl.gh")
+console.log(caesarCipher(caesarCipher("Always-Look-on-the-Bright-Side-of-Life", 5),-5));
