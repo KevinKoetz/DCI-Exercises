3dc9dab21ae71bab0089120f5c5dbcd56d6df6c1
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-10-26 11:48:15 +0200

    solved

--- index.ts
+++ index.ts
@@ -4,35 +4,17 @@ const line = (() => {
   return () => l(`_______________${num++}_______________`);
 })();
 
-/* Calculate the sum of natural number up to n
-Write a JavaScript program to compute the sum of an array of integers
-    const array = [1, 2, 3, 4, 5, 6]
-    sum(array)  // return 21 */
+/* Recursion
+Calculate factorial of n. Reminder n! = 1 * 2 * ... * n
+Write a JavaScript program to calculate the factorial of a number.
 
-function sum(arr: number[]): number{
-  if(arr.length === 1){
-    return arr[0]
-  }else{
-    return arr[0] + sum(arr.slice(1))
-  }
+In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example, 5! = 5 x 4 x 3 x 2 x 1 = 120
+
+    factorial(5)  // return 120 */
+
+function factorial(n: number) : number {
+  if(n === 0) return 1
+  return n * factorial(n-1)
 }
 
-console.log(sum([1, 2, 3, 4, 5, 6]));
-
-/**
- * Fibonacci with Map
- */
-
-function Fibonacci(n: bigint, map: Map<bigint,bigint> = new Map()): bigint{
-  if(map.get(n) !== undefined) return map.get(n) as bigint
-  if(n === 0n) return 0n
-  if(n === 1n) return 1n
-    const nthFib = Fibonacci(n-1n, map) + Fibonacci(n-2n, map)
-    map.set(n, nthFib)
-    return nthFib
-  
-}
- 
-
-
-console.log(Fibonacci(800n));
+console.log(factorial(5));
