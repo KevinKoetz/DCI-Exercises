e91aad52d4e40d81d73415c4e9bd84f385588ab6
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-10-26 12:03:00 +0200

    solved

--- index.ts
+++ index.ts
@@ -18,3 +18,43 @@ function factorial(n: number) : number {
 }
 
 console.log(factorial(5));
+
+/* 
+Write a function which can reverse string
+Write a JavaScript function that reverse a string. Sample Data and output: Example reverse("hello world") Expected Output: dlrow olleh
+    const string="hello world"
+    reverse(string)  // return dlrow olleh */
+
+function reverseString(str: string): string{
+  if(str.length <= 1) return str
+  return str[str.length - 1] + reverseString(str.slice(1,str.length - 1)) + str[0]
+}
+
+const string="hello world"
+console.log(reverseString("string")); // return dlrow olleh */
+
+/* Recursion
+Find the nth Fibonacci number
+Write a JavaScript program to get the first n Fibonacci numbers.
+
+Note: The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . . Each subsequent number is the sum of the previous two.
+
+fibonnaci(0)=> [0]
+fibonnaci(1)=> [0,1]
+fibonnaci(2)=> [0,1,1]
+fibonnaci(8)=> [0, 1, 1, 2, 3, 5, 8, 13,21]
+    fibonnaci(8) //  [0, 1, 1, 2, 3, 5, 8, 13,21]   */
+
+    function Fibonacci(n: bigint, map: Map<bigint,bigint> = new Map()): bigint{
+      if(map.get(n) !== undefined) return map.get(n) as bigint
+      if(n === 0n) return 0n
+      if(n === 1n) return 1n
+        const nthFib = Fibonacci(n-1n, map) + Fibonacci(n-2n, map)
+        map.set(n, nthFib)
+        return nthFib
+      
+    }
+     
+    
+    
+    console.log(Fibonacci(800n));
