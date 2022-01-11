3a4939902fd93d55facea3f34782705db7e00475
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-01-11 21:47:44 +0100

    Implement Basic App Markup and localStorage loading

--- spa-todolist/src/App.tsx
+++ spa-todolist/src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
+import {ITask} from "./common/types";
+import isITaskArray from './common/isITaskArray';
+import TaskList from './components/tasklist/TaskList';
 
 function App() {
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  useEffect(()=>{
+    const tasksFromLocalStorage = localStorage.getItem("tasks")
+    if(!tasksFromLocalStorage) return;
+    const tasks = JSON.parse(tasksFromLocalStorage) as unknown
+    if(!isITaskArray(tasks)) throw new Error(`Expected to get an ITask[] from localStorage but got: \n ${tasks}`)
+    setTasks(tasks);
+  }, [])
+
   return (
     <div className="App">
-     
+      <form>
+        <label htmlFor="">Define your task</label>
+        <input type="text" name="Task Text" />
+        <input type="button" value="Add a Task" />
+      </form>
+      <TaskList tasks={tasks}/>
     </div>
   );
 }
+
+
 
 export default App;
