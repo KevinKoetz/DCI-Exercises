6c5576bc7c58981c108f053b26a72b50f01b1d55
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-01-12 00:30:13 +0100

    Implemented Task Adding Logic

--- spa-todolist/src/App.tsx
+++ spa-todolist/src/App.tsx
@@ -3,9 +3,11 @@ import "./App.scss";
 import { ITask } from "./common/types";
 import isITaskArray from "./common/isITaskArray";
 import TaskList from "./components/tasklist/TaskList";
+import generateId from "./common/generateId";
 
 function App() {
   const [tasks, setTasks] = useState<ITask[]>([]);
+  const [taskText, setTaskText] = useState("")
 
   useEffect(() => {
     const tasksFromLocalStorage = localStorage.getItem("tasks");
@@ -16,7 +18,12 @@ function App() {
         `Expected to get an ITask[] from localStorage but got: \n ${tasks}`
       );
     setTasks(tasks);
+    generateId(Math.max(...tasks.map(task=>task.id)))
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks])
 
   const handleTaskDelete = (id: number) => {
     setTasks((tasks) => tasks.filter((task) => task.id !== id));
@@ -28,12 +35,18 @@ function App() {
     );
   };
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if(!taskText) return;
+    setTasks((tasks) => [...tasks, {text: taskText, done: false, id: generateId()}])
+  }
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="taskText">Define your task</label>
-        <input type="text" id="taskText" />
-        <input type="button" value="Add a Task" />
+        <input type="text" id="taskText" onChange={(e)=>setTaskText(e.target.value)} value={taskText}/>
+        <input type="submit" value="Add a Task" />
       </form>
       <TaskList
         tasks={tasks}
