a3b3b3c32006a70a97e250edcf8f29fbb4f27662
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-01-11 23:08:05 +0100

    Add Tests for Task Component

--- spa-todolist/src/components/task/Task.test.tsx
+++ spa-todolist/src/components/task/Task.test.tsx
@@ -1,7 +1,123 @@
-import React from 'react';
-import { render, screen} from '@testing-library/react';
-import Task from './Task';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "./Task.scss";
+import Task from "./Task";
+import userEvent from "@testing-library/user-event";
 
-test("Implement me!", () => {
-    throw new Error("Implement me!")
-})
+describe("Task should", () => {
+  test("render the Task Text", () => {
+    const taskText = "AbCDeF G";
+    render(
+      <Task
+        text={taskText}
+        done={false}
+        id={1}
+        onDelete={() => {}}
+        onDone={() => {}}
+      />
+    );
+    const renderedText = screen.getByText(taskText)
+    expect(renderedText).toBeInTheDocument()
+  });
+
+  test("render the Task Text with line-through if the task is done", () => {
+    const taskText = "AbCDeF G";
+    render(
+      <Task
+        text={taskText}
+        done={true}
+        id={1}
+        onDelete={() => {}}
+        onDone={() => {}}
+      />
+    );
+    const renderedText = screen.getByText(taskText)
+    expect(renderedText).toHaveStyle("text-decoration: line-through")
+  });
+
+  test("have the listitem role and task name", () => {
+    const taskText = "AbCDeF G";
+    render(
+      <Task
+        text={taskText}
+        done={false}
+        id={1}
+        onDelete={() => {}}
+        onDone={() => {}}
+      />
+    );
+    const task = screen.getByRole("listitem", {name: /task/i})
+    expect(task).toBeInTheDocument()
+  });
+
+  test("render a Delete task button", () => {
+    const taskText = "AbCDeF G";
+    render(
+      <Task
+        text={taskText}
+        done={false}
+        id={1}
+        onDelete={() => {}}
+        onDone={() => {}}
+      />
+    );
+    const deleteButton = screen.getByRole("button", {name: /delete task/i})
+    expect(deleteButton).toBeInTheDocument()
+  });
+
+  test("call the onDelete function with the task id when Delete task button is pressed", async () => {
+    const mockHandleDelete = jest.fn((id: number) => id)
+    const taskText = "AbCDeF G";
+    const id = 1;
+    render(
+      <Task
+        text={taskText}
+        done={false}
+        id={id}
+        onDelete={mockHandleDelete}
+        onDone={() => {}}
+      />
+    );
+    const deleteButton = screen.getByRole("button", {name: /delete task/i})
+    await userEvent.click(deleteButton)
+
+    expect(mockHandleDelete.mock.calls.length).toBe(1)
+    expect(mockHandleDelete.mock.calls[0][0]).toBe(id)
+  });
+
+  test("render a Set to Done button", () => {
+    const taskText = "AbCDeF G";
+    render(
+      <Task
+        text={taskText}
+        done={false}
+        id={1}
+        onDelete={() => {}}
+        onDone={() => {}}
+      />
+    );
+    const doneButton = screen.getByRole("button", {name: /set to done/i})
+    expect(doneButton).toBeInTheDocument()
+  });
+
+  test("call the onDone function with the task id when Done button is pressed", async () => {
+    const mockHandleDone = jest.fn((id: number) => id)
+    const taskText = "AbCDeF G";
+    const id = 1;
+    render(
+      <Task
+        text={taskText}
+        done={false}
+        id={id}
+        onDelete={() => {}}
+        onDone={mockHandleDone}
+      />
+    );
+    const doneButton = screen.getByRole("button", {name: /set to done/i})
+    await userEvent.click(doneButton)
+
+    expect(mockHandleDone.mock.calls.length).toBe(1)
+    expect(mockHandleDone.mock.calls[0][0]).toBe(id)
+  });
+
+});
