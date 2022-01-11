8e86716d2eb9e0cf61022baf9c2a60f84a51a273
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-01-12 00:28:43 +0100

    Added generateId Helper Function

---
+++ spa-todolist/src/common/generateId.test.ts
@@ -0,0 +1,17 @@
+import generateId from "./generateId";
+
+describe("generateId should", () => {
+    test("return consecutive increasing numbers, starting from 0 each time it is called",() => {
+        expect(generateId()).toBe(0)
+        expect(generateId()).toBe(1)
+        expect(generateId()).toBe(2)
+        expect(generateId()).toBe(3)
+    })
+
+    test("return the overwrite value when called with one and then increace from there",() => {
+        expect(generateId(20)).toBe(20)
+        expect(generateId()).toBe(21)
+        expect(generateId()).toBe(22)
+        expect(generateId()).toBe(23)
+    })
+})
