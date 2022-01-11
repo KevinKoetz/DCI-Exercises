cda98eff47d6f20fdcef8d12016e0f6efa390e36
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-01-11 11:27:48 +0100

    Initialize App

---
+++ spa-todolist/src/App.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('To be implemented', () => {
+  throw new Error("Implement Tests!")
+});
