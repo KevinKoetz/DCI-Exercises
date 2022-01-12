46339cbdab5ac025c08eb7776156969e78f278bb
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-01-12 12:25:18 +0100

    Changed from exporting id generator to exporting the HoF thats creates the generator, due to strange behavior in the browser.

--- spa-todolist/src/common/generateId.ts
+++
@@ -1,9 +0,0 @@
-const generateId = (() => {
-    let id = 0;
-    return (overwrite?: number) => {
-      id = overwrite ? overwrite : id
-      return id++
-    }
-  })()
-
-export default generateId
