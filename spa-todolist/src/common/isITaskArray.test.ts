30624eda0fb16e1cd096e10ac51082c9eb9b1f6a
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-01-12 11:07:56 +0100

    added tests to improve coverage

--- spa-todolist/src/common/isITaskArray.test.ts
+++ spa-todolist/src/common/isITaskArray.test.ts
@@ -12,6 +12,8 @@ describe("isITaskArray should", () => {
     })
 
     test("return false if called with an Array that containst a non-ITask type element", () => {
+        expect(isITaskArray(["no object"])).toStrictEqual(false)
+        expect(isITaskArray([null])).toStrictEqual(false)
         expect(isITaskArray([{}])).toStrictEqual(false)
         expect(isITaskArray([{text: "Hello", done: true}, 5])).toStrictEqual(false)
         expect(isITaskArray([{text: "Hello", done: true},{}])).toStrictEqual(false)
@@ -19,6 +21,8 @@ describe("isITaskArray should", () => {
         expect(isITaskArray([{done: false}])).toStrictEqual(false)
         expect(isITaskArray([{id: false}])).toStrictEqual(false)
         expect(isITaskArray([{text:"Test",done:false,id:null},{text:"Test2",done:false,id:null}])).toStrictEqual(false)
+        expect(isITaskArray([{text:true,done:false,id:2}])).toStrictEqual(false)
+        expect(isITaskArray([{text:"Test",done:"false",id:2}])).toStrictEqual(false)
     })
 
     test("return true if called with an empty Array", () => {
